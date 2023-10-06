85d87557a921c308eb52e92a1dfa6b944a1dbfd5
Brian Teeters <brianteetersdesign@gmail.com>
2023-10-06 11:12:36 -0400

    Updated main to import and use settings.py

---
+++ settings.py
@@ -0,0 +1,6 @@
+class Settings:
+    """A class to store all settings for Alien Invasion"""
+    # Screen settings
+    self.screen_width(1200)
+    self.screen_height(700)
+    self.bg_color(230,230,230)
