491b7666248829d17f3b94d4705dacb280638fd5
Brian Teeters <brianteetersdesign@gmail.com>
2023-10-13 07:28:56 -0400

    Added alien-player collision

--- alien_invasion.py
+++ alien_invasion.py
@@ -132,6 +132,10 @@ class AlienInvasion:
         self._check_fleet_edges()
         self.aliens.update()
 
+        # Look for alien-player collisions.
+        if pygame.sprite.spritecollideany(self.ship, self.aliens):
+            print('Ship hit!')
+
     def _check_fleet_edges(self):
         """Respond appropriately if any aliens have reached an edge."""
         for alien in self.aliens.sprites():
