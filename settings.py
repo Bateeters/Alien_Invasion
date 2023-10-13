290c42d6deb4b44767c8d552d3b80eed962ac1f3
Brian Teeters <brianteetersdesign@gmail.com>
2023-10-13 06:56:13 -0400

    Move aliens to the right.

--- settings.py
+++ settings.py
@@ -17,3 +17,6 @@ class Settings:
         self.bullet_height = 15
         self.bullet_color = (230,230,230)
         self.bullets_allowed = 7
+
+        # Alien settings
+        self.alien_speed = 1.0
