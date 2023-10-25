090449d670aaab61d9a07ad22803007ec003168a
Brian Teeters <brianteetersdesign@gmail.com>
2023-10-25 13:22:56 -0400

    Restart speed changes

--- alien_invasion.py
+++ alien_invasion.py
@@ -208,7 +208,8 @@ class AlienInvasion:
         """Start game when the player clicks Play."""
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.game_active:
-            # Reset the game.
+            # Reset the game settings.
+            self.settings.initialize_dynamic_settings()
             self.stats.reset_stats()
             self.game_active = True
 
